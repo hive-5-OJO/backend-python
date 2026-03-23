341b406b806ae7b2031e342ba889e08f2234c3e8
0andrich <rlrpdj@gmail.com>
2026-03-23 16:43:23 +0900

    fix:추천 로직 간소화로 aws 리소스 맞추어 진행

--- app/model/recommendation.py
+++ app/model/recommendation.py
@@ -228,162 +228,267 @@ def get_all_recommendations(ojo_engine, analysis_engine):
 
     return "[AI 추천] 전체 추천 성공"
 
+# def get_recommendations(member_id, ojo_engine):
+#     query = """
+#     SELECT 
+#         m.member_id, m.gender, m.birth_date, m.region, m.household_type, m.created_at as join_date,
+#         fu.total_usage_amount, fu.usage_active_days_30d, fu.usage_peak_hour,
+#         fm.avg_monthly_bill, fm.total_revenue, fm.payment_delay_count,
+#         fc.top_consult_category, fc.total_consult_count,
+#         -- an.rfm_score, an.type as segment, an.ltv,
+#         -- cp.churn_score, cp.risk_grade, 
+#         sp.product_id as current_product_id,
+#         p.product_name as current_product_name
+#     FROM member m
+#     JOIN feature_usage fu ON m.member_id = fu.member_id
+#     JOIN feature_monetary fm ON m.member_id = fm.member_id
+#     JOIN feature_consultation fc ON m.member_id = fc.member_id
+#     -- JOIN analysis an ON m.member_id = an.member_id
+#     -- LEFT JOIN churn_prediction cp ON m.member_id = cp.member_id
+#     LEFT JOIN subscription_period sp ON m.member_id = sp.member_id AND sp.status = 'ACTIVE'
+#     LEFT JOIN product p ON sp.product_id = p.product_id
+#     """
+#     df = pd.read_sql(query, con=ojo_engine)
+#     if df[df['member_id'] == member_id].empty: return []
+#     products = pd.read_sql("SELECT * FROM product", con=ojo_engine)
+
+#     current_year = datetime.now().year
+#     df['age'] = current_year - pd.to_datetime(df['birth_date']).dt.year
+
+#     # 대상 고객 정보 추출
+#     target_user = df[df['member_id'] == member_id].iloc[0]
+#     target_data_limit = DATA_LIMIT_MAP.get(target_user['current_product_name'], 0)
+#     age = target_user['age']
+
+#     features = ['household_type', 'total_usage_amount', 'usage_active_days_30d', 'avg_monthly_bill', 
+#                 'usage_peak_hour', 'payment_delay_count', 'total_consult_count', 'age',]
+#                 # 'ltv', 'rfm_score',]
+#     scaler = StandardScaler()
+#     df_scaled = scaler.fit_transform(df[features].fillna(0))
+
+#     target_idx = df[df['member_id'] == member_id].index[0]
+#     user_sim = cosine_similarity(df_scaled[target_idx].reshape(1, -1), df_scaled)[0]
+#     user_sim_series = pd.Series(user_sim, index=df['member_id'])
+
+#     # 대상 고객과 가장 유사한 상위 10명 추출
+#     similar_users = user_sim_series.sort_values(ascending=False).iloc[1:11].index
+
+#     # 유사한 사용자들이 가장 많이 사용하는 상품 집계 (현재 내 상품 제외)
+#     # target_user_product = df[df['member_id'] == member_id]['current_product_id'].values[0]
+#     # 
+#     # recommended_products = df[df['member_id'].isin(similar_users)]
+#     # recommend_list = recommended_products[recommended_products['current_product_id'] != target_user_product]
+#     similar_user_products = df[df['member_id'].isin(similar_users)]['current_product_id'].unique()
+
+#     recommendations = []
+
+#     for _, p in products.iterrows():
+#         # 현재 사용자가 사용하고 있거나 사용하고 있는 거보다 데이터 한도 낮은거 빼기
+#         if p['product_name'] == target_user['current_product_name']: continue
+#         if DATA_LIMIT_MAP.get(p['product_name'], 0) < target_data_limit : continue
+        
+#         # 유사도 : 비즈니스 = 3:7
+#         scores = 30 if p['product_id'] in similar_user_products else 0 
+#         reasons = []
+
+#         # 연령
+#         age_match = True
+#         if '키즈' in p['product_name']:
+#             if age <= 12:
+#                 scores += 20    
+#                 reasons.append(f"어린이 고객님(만 {age}세)을 위한 맞춤 요금제입니다.")
+#             else: age_match = False
+#         elif '청소년' in p['product_name']:
+#             if 12 < age <= 18:
+#                 scores += 20
+#                 reasons.append(f"청소년 고객님(만 {age}세)을 위한 맞춤 요금제입니다.")
+#             else: age_match = False
+#         elif '유쓰' in p['product_name']:
+#             if 18 < age <= 34:
+#                 scores += 20
+#                 reasons.append(f"청년 고객님(만 {age}세)을 위한 맞춤 요금제입니다.")
+#             else: age_match = False
+#         elif '시니어' in p['product_name']:
+#             if age >= 65:
+#                 scores += 20
+#                 reasons.append(f"실버 고객님(만 {age}세)을 위한 맞춤 요금제입니다.") 
+#             else: age_match = False
+#         if not age_match: continue
+
+#         # 최대 데이터 사용량의 90% 이상 사용자에게 자신의 사용금액에 추가 2만원 이내의 초과 상품 추천하기
+#         if target_data_limit > 0 and (target_user['total_usage_amount'] / target_data_limit) > 0.9:
+#             if target_user['avg_monthly_bill'] < p['price'] <= target_user['avg_monthly_bill'] + 20000:
+#                 scores += 25
+#         reasons.append("데이터 사용 패턴에 맞춰 한 단계 높은 요금제를 추천합니다. ")
+
+#         # 가족 구성 3인 이상이면 결합 상품 추천 => db에 lg u+ 결합 상품 추가(U+투게더 결합, 참 쉬운 가족 결합 ,신혼플러스 결합, 참 쉬운 케이블 가족 결합)
+#         if any(k in p['product_name'] for k in ['가족', '결합', '투게더']):
+#             if target_user['household_type'] >= 3:
+#                 scores += 20
+#                 reasons.append("다인 가구 맞춤 결합 상품으로 통신비를 절감해보세요. ")
+
+#         # 가입 기간 - 15점
+#         join_date = pd.to_datetime(target_user['join_date'])
+#         calculation_base_date = datetime(datetime.now().year, 11, 30)
+#         tenure_years = (calculation_base_date - join_date).days // 365
+        
+#         # 장기 고객 (2년, 5년, 10년 이상) - 결합 할인 및 VIP 혜택 강조
+#         # 장기 고객 전용 혜택 필요(현재 존재하지 않는 거 같음)
+#         # 실제 로직은 약간 변경 필요
+#         if tenure_years >= 10:
+#             print("")
+#         elif tenure_years >= 5:
+#             print("")
+#         elif tenure_years >= 2:
+#             print("")
+
+#         # 세그먼트 - LOST, RISK, COMMON , LOYAL, vip 
+#         # if p['product_category'] == 'ADDON_SERVICE':
+#         #     if target_user['rfm_type'] == 'VIP':
+#         #         scores += 30
+#         #         reasons.append("VIP 고객님께 제공되는 전용 라이프스타일 혜택입니다. ")
+            
+#         #     # 일반 고객에게는 '체험형' 부가서비스 추천 (저렴한 것 위주)
+#         #     if (target_user['rfm_type'] != 'VIP') & (p['price'] < 5000):
+#         #         scores += 20, 0
+#         #         reasons.append("부담 없는 가격으로 이용 가능한 인기 부가서비스입니다. ")
+#         # if (target_user['type'] == 'RISK') & (target_user['ltv'] > df['ltv'].quantile(0.8)):
+#         #     if (p['price'] < target_user['current_base_price']):
+#         #         scores += 25
+#         #         reasons.append("장기 우수 고객님을 위한 요금 다이어트 제안입니다. ")    
+
+#         # 연체
+#         if target_user['payment_delay_count'] >= 1:
+#             if (p['price'] < target_user['avg_monthly_bill']) & (p['product_category'] == 'BASE'):
+#                 scores += 20    
+#                 reasons.append("가계 통신비 절감을 위한 실속형 요금제입니다.")
+
+#         # 상담 카테고리
+#         if '로밍' in p['product_name'] and target_user['last_consult_category'] == 18: # 로밍 관련 ID 
+#             scores += 50
+#             reasons.append("최근 로밍 상담을 바탕으로 가장 인기 있는 로밍 상품을 추천합니다. ")
+#         # if p['product_category'] == 'BASE' and p['price'] < 50000 and (target_user['churn_score'] > 0.8) | (target_user['last_consult_category'] == 19): # 해지 상담
+#         #     scores += 40
+#         #     reasons.append("고객님을 위한 특별 할인 요금제와 혜택을 확인해보세요. ")    
+
+#         if scores >= 30:
+#             recommendations.append({
+#                 'member_id': member_id,
+#                 'recommended_product': p['product_name'],
+#                 'price' : p['price'],
+#                 'score': scores,
+#                 'reason': ", ".join(reasons) if reasons else "고객님을 위한 맞춤 상품이 없습니다"
+#             })
+  
+#     if not recommendations: return []
+#     final_res = pd.DataFrame(recommendations)
+    
+#     final_res = final_res.sort_values(by='score', ascending=False)
+#     final_res['rank'] = range(1, len(final_res) + 1)
+#     final_res['created_at'] = datetime.now().strftime('%Y-%m-%d')
+#     return final_res.head(3).to_dict(orient='records')
+
 def get_recommendations(member_id, ojo_engine):
-    query = """
+    # start_time = time.time()
+
+    target_query = f"SELECT * FROM member WHERE member_id = {member_id}"
+    target_df = pd.read_sql(target_query, con=ojo_engine)
+    if target_df.empty: return []
+    
+    target_user_info = target_df.iloc[0]
+    
+    # 지역 기반 비교 후보군 10,000명 샘플링 (AWS 최적화)
+    pool_query = f"""
     SELECT 
-        m.member_id, m.gender, m.birth_date, m.region, m.household_type, m.created_at as join_date,
+        m.member_id, m.birth_date, m.household_type,
         fu.total_usage_amount, fu.usage_active_days_30d, fu.usage_peak_hour,
-        fm.avg_monthly_bill, fm.total_revenue, fm.payment_delay_count,
-        fc.top_consult_category, fc.total_consult_count,
-        -- an.rfm_score, an.type as segment, an.ltv,
-        -- cp.churn_score, cp.risk_grade, 
+        fm.avg_monthly_bill, fc.total_consult_count,
         sp.product_id as current_product_id,
         p.product_name as current_product_name
     FROM member m
     JOIN feature_usage fu ON m.member_id = fu.member_id
     JOIN feature_monetary fm ON m.member_id = fm.member_id
     JOIN feature_consultation fc ON m.member_id = fc.member_id
-    -- JOIN analysis an ON m.member_id = an.member_id
-    -- LEFT JOIN churn_prediction cp ON m.member_id = cp.member_id
     LEFT JOIN subscription_period sp ON m.member_id = sp.member_id AND sp.status = 'ACTIVE'
     LEFT JOIN product p ON sp.product_id = p.product_id
+    WHERE m.region = '{target_user_info['region']}' AND m.member_id != {member_id}
+    LIMIT 10000 
     """
-    df = pd.read_sql(query, con=ojo_engine)
-    if df[df['member_id'] == member_id].empty: return []
-    products = pd.read_sql("SELECT * FROM product", con=ojo_engine)
+    df = pd.read_sql(pool_query, con=ojo_engine)
+    
+    df = pd.concat([df, target_df], ignore_index=True)
 
+    # 데이터 전처리
     current_year = datetime.now().year
     df['age'] = current_year - pd.to_datetime(df['birth_date']).dt.year
+    df = df.fillna(0)
 
-    # 대상 고객 정보 추출
-    target_user = df[df['member_id'] == member_id].iloc[0]
-    target_data_limit = DATA_LIMIT_MAP.get(target_user['current_product_name'], 0)
-    age = target_user['age']
-
-    features = ['household_type', 'total_usage_amount', 'usage_active_days_30d', 'avg_monthly_bill', 
-                'usage_peak_hour', 'payment_delay_count', 'total_consult_count', 'age',]
-                # 'ltv', 'rfm_score',]
-    scaler = StandardScaler()
-    df_scaled = scaler.fit_transform(df[features].fillna(0))
-
-    target_idx = df[df['member_id'] == member_id].index[0]
-    user_sim = cosine_similarity(df_scaled[target_idx].reshape(1, -1), df_scaled)[0]
-    user_sim_series = pd.Series(user_sim, index=df['member_id'])
-
-    # 대상 고객과 가장 유사한 상위 10명 추출
-    similar_users = user_sim_series.sort_values(ascending=False).iloc[1:11].index
-
-    # 유사한 사용자들이 가장 많이 사용하는 상품 집계 (현재 내 상품 제외)
-    # target_user_product = df[df['member_id'] == member_id]['current_product_id'].values[0]
-    # 
-    # recommended_products = df[df['member_id'].isin(similar_users)]
-    # recommend_list = recommended_products[recommended_products['current_product_id'] != target_user_product]
-    similar_user_products = df[df['member_id'].isin(similar_users)]['current_product_id'].unique()
-
-    recommendations = []
-
-    for _, p in products.iterrows():
-        # 현재 사용자가 사용하고 있거나 사용하고 있는 거보다 데이터 한도 낮은거 빼기
-        if p['product_name'] == target_user['current_product_name']: continue
-        if DATA_LIMIT_MAP.get(p['product_name'], 0) < target_data_limit : continue
-        
-        # 유사도 : 비즈니스 = 3:7
-        scores = 30 if p['product_id'] in similar_user_products else 0 
-        reasons = []
-
-        # 연령
-        age_match = True
-        if '키즈' in p['product_name']:
-            if age <= 12:
-                scores += 20    
-                reasons.append(f"어린이 고객님(만 {age}세)을 위한 맞춤 요금제입니다.")
-            else: age_match = False
-        elif '청소년' in p['product_name']:
-            if 12 < age <= 18:
-                scores += 20
-                reasons.append(f"청소년 고객님(만 {age}세)을 위한 맞춤 요금제입니다.")
-            else: age_match = False
-        elif '유쓰' in p['product_name']:
-            if 18 < age <= 34:
-                scores += 20
-                reasons.append(f"청년 고객님(만 {age}세)을 위한 맞춤 요금제입니다.")
-            else: age_match = False
-        elif '시니어' in p['product_name']:
-            if age >= 65:
-                scores += 20
-                reasons.append(f"실버 고객님(만 {age}세)을 위한 맞춤 요금제입니다.") 
-            else: age_match = False
-        if not age_match: continue
-
-        # 최대 데이터 사용량의 90% 이상 사용자에게 자신의 사용금액에 추가 2만원 이내의 초과 상품 추천하기
-        if target_data_limit > 0 and (target_user['total_usage_amount'] / target_data_limit) > 0.9:
-            if target_user['avg_monthly_bill'] < p['price'] <= target_user['avg_monthly_bill'] + 20000:
-                scores += 25
-        reasons.append("데이터 사용 패턴에 맞춰 한 단계 높은 요금제를 추천합니다. ")
-
-        # 가족 구성 3인 이상이면 결합 상품 추천 => db에 lg u+ 결합 상품 추가(U+투게더 결합, 참 쉬운 가족 결합 ,신혼플러스 결합, 참 쉬운 케이블 가족 결합)
-        if any(k in p['product_name'] for k in ['가족', '결합', '투게더']):
-            if target_user['household_type'] >= 3:
-                scores += 20
-                reasons.append("다인 가구 맞춤 결합 상품으로 통신비를 절감해보세요. ")
-
-        # 가입 기간 - 15점
-        join_date = pd.to_datetime(target_user['join_date'])
-        calculation_base_date = datetime(datetime.now().year, 11, 30)
-        tenure_years = (calculation_base_date - join_date).days // 365
-        
-        # 장기 고객 (2년, 5년, 10년 이상) - 결합 할인 및 VIP 혜택 강조
-        # 장기 고객 전용 혜택 필요(현재 존재하지 않는 거 같음)
-        # 실제 로직은 약간 변경 필요
-        if tenure_years >= 10:
-            print("")
-        elif tenure_years >= 5:
-            print("")
-        elif tenure_years >= 2:
-            print("")
-
-        # 세그먼트 - LOST, RISK, COMMON , LOYAL, vip 
-        # if p['product_category'] == 'ADDON_SERVICE':
-        #     if target_user['rfm_type'] == 'VIP':
-        #         scores += 30
-        #         reasons.append("VIP 고객님께 제공되는 전용 라이프스타일 혜택입니다. ")
-            
-        #     # 일반 고객에게는 '체험형' 부가서비스 추천 (저렴한 것 위주)
-        #     if (target_user['rfm_type'] != 'VIP') & (p['price'] < 5000):
-        #         scores += 20, 0
-        #         reasons.append("부담 없는 가격으로 이용 가능한 인기 부가서비스입니다. ")
-        # if (target_user['type'] == 'RISK') & (target_user['ltv'] > df['ltv'].quantile(0.8)):
-        #     if (p['price'] < target_user['current_base_price']):
-        #         scores += 25
-        #         reasons.append("장기 우수 고객님을 위한 요금 다이어트 제안입니다. ")    
-
-        # 연체
-        if target_user['payment_delay_count'] >= 1:
-            if (p['price'] < target_user['avg_monthly_bill']) & (p['product_category'] == 'BASE'):
-                scores += 20    
-                reasons.append("가계 통신비 절감을 위한 실속형 요금제입니다.")
-
-        # 상담 카테고리
-        if '로밍' in p['product_name'] and target_user['last_consult_category'] == 18: # 로밍 관련 ID 
-            scores += 50
-            reasons.append("최근 로밍 상담을 바탕으로 가장 인기 있는 로밍 상품을 추천합니다. ")
-        # if p['product_category'] == 'BASE' and p['price'] < 50000 and (target_user['churn_score'] > 0.8) | (target_user['last_consult_category'] == 19): # 해지 상담
-        #     scores += 40
-        #     reasons.append("고객님을 위한 특별 할인 요금제와 혜택을 확인해보세요. ")    
-
-        if scores >= 30:
-            recommendations.append({
-                'member_id': member_id,
-                'recommended_product': p['product_name'],
-                'price' : p['price'],
-                'score': scores,
-                'reason': ", ".join(reasons) if reasons else "고객님을 위한 맞춤 상품이 없습니다"
-            })
-  
-    if not recommendations: return []
-    final_res = pd.DataFrame(recommendations)
+    features = ['household_type', 'total_usage_amount', 'usage_active_days_30d', 
+                'avg_monthly_bill', 'usage_peak_hour', 'total_consult_count', 'age']
     
-    final_res = final_res.sort_values(by='score', ascending=False)
-    final_res['rank'] = range(1, len(final_res) + 1)
-    final_res['created_at'] = datetime.now().strftime('%Y-%m-%d')
-    return final_res.head(3).to_dict(orient='records')
+    scaler = StandardScaler()
+    scaled_features = scaler.fit_transform(df[features])
+    
+    # 대상 유저 인덱스 찾기
+    target_idx = df[df['member_id'] == member_id].index[0]
+    target_age = df.loc[target_idx, 'age']
+    target_household = df.loc[target_idx, 'household_type']
+    
+    # 유사도 계산 및 상위 10명 추출
+    user_sim = cosine_similarity(scaled_features[target_idx].reshape(1, -1), scaled_features)[0]
+    df['similarity'] = user_sim
+    similar_users = df[df['member_id'] != member_id].sort_values(by='similarity', ascending=False).head(10)
+
+    # 현재 유저가 이미 쓰고 있는 상품은 제외
+    target_product_id = target_user_info.get('current_product_id')
+    top_products_counts = similar_users[similar_users['current_product_id'] != target_product_id]['current_product_id'].value_counts()
+    
+    if top_products_counts.empty: return []
+
+    # 최종 추천 리스트 구성 및 가산점 부여
+    recommend_ids = top_products_counts.index.tolist()
+    product_info = pd.read_sql(f"SELECT product_id, product_name, price FROM product WHERE product_id IN ({','.join(map(str, recommend_ids))})", con=ojo_engine)
+    
+    recommendations = []
+    for _, p in product_info.iterrows():
+        p_name = p['product_name']
+        # 빈도수 점수 (1명당 10점)
+        freq = top_products_counts.get(p['product_id'], 0)
+        score = freq * 10
+        
+        # 비즈니스 가산점
+        reasons = [f"유사 패턴 고객 {freq}명이 선택한 인기 상품입니다."]
+        
+        # 연령대 가산점
+        if '키즈' in p_name and target_age <= 12: 
+            score += 20
+            reasons.append("어린이 고객님 맞춤 혜택이 포함되었습니다.")
+        elif '청소년' in p_name and 12 < target_age <= 18: 
+            score += 20
+            reasons.append("청소년 고객님 맞춤 혜택이 포함되었습니다.")    
+        elif '유쓰' in p_name and 18 < target_age <= 34: 
+            score += 20
+            reasons.append("청년 고객님 맞춤 혜택이 포함되었습니다.")
+        elif '시니어' in p_name and target_age >= 65:
+            score += 20
+            reasons.append("실버 고객님 전용 우대 상품입니다.")
+            
+        # 가구원수 가산점
+        if any(k in p_name for k in ['가족', '결합', '투게더']) and target_household >= 3:
+            score += 15
+            reasons.append("다인 가구 결합 시 통신비 절감이 가능합니다.")
+
+        recommendations.append({
+            'member_id': int(member_id),
+            'recommended_product': p_name,
+            'price': int(p['price']),
+            'score': int(score),
+            'reason': " ".join(reasons),
+            'created_at': datetime.now().strftime('%Y-%m-%d')
+        })
+
+    # end_time = time.time()
+    # print(f"[추천 완료] 소요시간: {end_time - start_time:.2f}초", flush=True)
+
+    # 점수 높은 순 정렬 후 상위 3개
+    return sorted(recommendations, key=lambda x: x['score'], reverse=True)[:3]
